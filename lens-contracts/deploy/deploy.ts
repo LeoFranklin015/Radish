4a4fe5d458848728d8591eac3219a1eedd6bb925
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-01-05 18:25:34 +0530

    Contract Changes and Subgraph updated

--- lens-contracts/deploy/deploy.ts
+++ lens-contracts/deploy/deploy.ts
@@ -34,18 +34,18 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   //   console.log("NoToken deployed to:", await noToken.getAddress());
 
   // Step 4: Deploy RadishCore
-  const radishCoreArtifact = await deployer.loadArtifact("RadishCore");
-  const radishCore = await deployer.deploy(radishCoreArtifact, [
-    // await mockUSDC.getAddress(),
-    // await yesToken.getAddress(),
-    // await noToken.getAddress(),
-    "0x88b2DFA23505360b054baE96774b4e7C2D83ca08",
-    "0xB7a6D76A6448B35aCfC8aDbe5E9D7FDBB3c1ebC2",
-    "0x939a843E55da283eD4209cF9a4122F37Aff4E6cb",
-  ]);
-  console.log("RadishCore deployed to:", await radishCore.getAddress());
-  const radishCoreAddress = await radishCore.getAddress();
-  console.log("RadishCore address:", radishCoreAddress);
+  // const radishCoreArtifact = await deployer.loadArtifact("RadishCore");
+  // const radishCore = await deployer.deploy(radishCoreArtifact, [
+  //   // await mockUSDC.getAddress(),
+  //   // await yesToken.getAddress(),
+  //   // await noToken.getAddress(),
+  //   "0x88b2DFA23505360b054baE96774b4e7C2D83ca08",
+  //   "0xB7a6D76A6448B35aCfC8aDbe5E9D7FDBB3c1ebC2",
+  //   "0x939a843E55da283eD4209cF9a4122F37Aff4E6cb",
+  // ]);
+  // console.log("RadishCore deployed to:", await radishCore.getAddress());
+  // const radishCoreAddress = await radishCore.getAddress();
+  // console.log("RadishCore address:", radishCoreAddress);
 
   // Step 5: Transfer ownership of YesToken and NoToken to RadishCore
   //   const transferOwnershipTx = yesToken.getFunction("transferOwnership");
@@ -60,30 +60,33 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   //   console.log("Ownership of NoToken transferred to RadishCore");
 
   // Verify contract
-  await hre.run("verify:verify", {
-    address: radishCoreAddress,
-    constructorArguments: [
-      mockUSDC.getAddress(),
-      yesToken.getAddress(),
-      noToken.getAddress(),
-    ],
-  });
+  // await hre.run("verify:verify", {
+  //   address: radishCoreAddress,
+  //   constructorArguments: [
+  //     // mockUSDC.getAddress(),
+  //     // yesToken.getAddress(),
+  //     // noToken.getAddress(),
+  //     "0x88b2DFA23505360b054baE96774b4e7C2D83ca08",
+  //     "0xB7a6D76A6448B35aCfC8aDbe5E9D7FDBB3c1ebC2",
+  //     "0x939a843E55da283eD4209cF9a4122F37Aff4E6cb",
+  //   ],
+  // });
 
   // Verify contract
   await hre.run("verify:verify", {
-    address: await mockUSDC.getAddress(),
+    address: "0x88b2DFA23505360b054baE96774b4e7C2D83ca08",
     constructorArguments: ["USD Coin", "USDC"],
   });
 
   // Verify contract
   await hre.run("verify:verify", {
-    address: await yesToken.getAddress(),
+    address: "0xB7a6D76A6448B35aCfC8aDbe5E9D7FDBB3c1ebC2",
     constructorArguments: [await wallet.getAddress()],
   });
 
   // Verify contract
   await hre.run("verify:verify", {
-    address: await noToken.getAddress(),
+    address: "0x939a843E55da283eD4209cF9a4122F37Aff4E6cb",
     constructorArguments: [await wallet.getAddress()],
   });
 
